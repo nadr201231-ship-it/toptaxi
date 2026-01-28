195f2f5c4e0e6a1cea6e9e9f193a0fb406521d59
Abdullah <nadr201231@gmail.com>
2026-01-28 18:10:21 +0300

    fix: babel config for reanimated

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
-    '@babel/plugin-proposal-optional-chaining' ,
+    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     [
       'module-resolver',
@@ -30,8 +29,8 @@ module.exports = {
           '@components': './src/components',
           '@api': './src/api',
           '@App': './App',
-        }
-      }
+        },
+      },
     ],
     [
       'module:react-native-dotenv',
@@ -42,7 +41,10 @@ module.exports = {
         whitelist: null,
         safe: false,
         allowUndefined: true,
-      }
+      },
     ],
+
+    // ⚠️ لازم يكون آخر شيء
+    'react-native-reanimated/plugin',
   ],
 };
