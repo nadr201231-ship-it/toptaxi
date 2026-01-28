0c338af7e03d14c680500625e3daf36c19f111aa
Abdullah <nadr201231@gmail.com>
2026-01-28 18:27:08 +0300

    fix: remove deprecated babel proposal plugins

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
+    'react-native-reanimated/plugin',
     [
       'module-resolver',
       {
@@ -29,22 +28,16 @@ module.exports = {
           '@components': './src/components',
           '@api': './src/api',
           '@App': './App',
-        },
-      },
+        }
+      }
     ],
     [
       'module:react-native-dotenv',
       {
         moduleName: '@env',
         path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
         allowUndefined: true,
-      },
+      }
     ],
-
-    // ⚠️ لازم يكون آخر شيء
-    'react-native-reanimated/plugin',
   ],
 };
