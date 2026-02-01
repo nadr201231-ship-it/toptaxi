a15c39765d96bd688842bf536cdff1569557362d
Abdullah <nadr201231@gmail.com>
2026-02-01 09:45:03 +0300

    Disable reanimated babel plugin in Android release CI

--- babel.config.js
+++ babel.config.js
@@ -1,43 +1,52 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        root: ['./src/'],
-        extensions: [
-          '.ios.ts',
-          '.android.ts',
-          '.ts',
-          '.ios.tsx',
-          '.android.tsx',
-          '.tsx',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-        alias: {
-          '@src': './src',
-          '@screens': './src/screens',
-          '@theme': './src/themes',
-          '@style': './src/styles',
-          '@utils': './src/utils',
-          '@assets': './src/assets',
-          '@commonComponent': './src/commonComponent',
-          '@components': './src/components',
-          '@api': './src/api',
-          '@App': './App',
-        }
-      }
+module.exports = function (api) {
+  api.cache(true);
+
+  const isReleaseCI = process.env.BUILD_RELEASE === 'true';
+
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      // ❌ عطّل reanimated plugin في Release CI فقط
+      ...(isReleaseCI ? [] : ['react-native-reanimated/plugin']),
+
+      [
+        'module-resolver',
+        {
+          root: ['./src/'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '@src': './src',
+            '@screens': './src/screens',
+            '@theme': './src/themes',
+            '@style': './src/styles',
+            '@utils': './src/utils',
+            '@assets': './src/assets',
+            '@commonComponent': './src/commonComponent',
+            '@components': './src/components',
+            '@api': './src/api',
+            '@App': './App',
+          },
+        },
+      ],
+
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: true,
+        },
+      ],
     ],
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: true,
-      }
-    ],
-  ],
+  };
 };
